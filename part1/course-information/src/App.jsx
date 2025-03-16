ed56dc11da7a5ab749059025b28df4dd056856a1
Kirunda Brian <kirundab@gmail.com>
2025-03-16 19:58:25 +0300

    2.1: Course information step 6

--- part1/course-information/src/App.jsx
+++ part1/course-information/src/App.jsx
@@ -42,27 +42,33 @@ const App = () => {
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
+  const Course = ({ course }) => {
+    return (
+      <div>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+      </div>
+    );
+  }
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 export default App;
